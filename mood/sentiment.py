d1b5ba7a18e1b87250f64cd832b0007bcc6f56f4
Thor Whalen <1906276+thorwhalen@users.noreply.github.com>
2025-02-19 10:09:57 -0500

    refactor: remove Sphinx documentation files and related configurations

--- mood/sentiment.py
+++ mood/sentiment.py
@@ -1,6 +1,5 @@
 """Sentiment analysis tools """
 
-from oa import ask
 import json
 from dol import Pipe
 
@@ -10,6 +9,8 @@ from mood.util import add_egress
 @add_egress(json.loads)
 def sentiment_analysis(headlines):
     """Get sentiment analysis for headlines."""
+    from oa import ask
+
     if not isinstance(headlines, str):  # Pattern: ingress
         headlines = '\n\n'.join(
             headlines
